08c69ee92dcaf8f13a5e39c079b16d225d5c37b6
Lucas Chavez <luck9217@gmail.com>
2020-09-14 01:20:02 -0300

    formulario React CRUD

---
+++ src/components/UserTable.jsx
@@ -0,0 +1,70 @@
+import React from 'react';//imr
+
+const  UserTable= (props) => {//sfc
+    //console.log(props.users)
+    return (  
+
+            <table>
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+
+                {
+                props.users.length > 0 ? (
+                  props.users.map(user => (
+                                                <tr key={user.id}>
+                                                    <td>{user.name}</td>
+                                                    <td>{user.username}</td>
+                                                    <td>
+                                                    <button 
+                                                    
+                                                        className="button muted-button"
+                                                        
+                                                        onClick={() => {props.editRow(user)}}
+                                                        
+                                                        >
+                                                        
+                                                        
+                                                        Edit
+                                                    
+                                                    </button>
+                                                   
+                                                   
+                                                    <button 
+                                                    
+                                                        className="button muted-button"
+                                                    
+                                                        onClick={() => {props.deleteUser(user.id)}}
+                                                    
+                                                    
+                                                    >
+                                                        Delete
+                                                        
+                                                    </button>
+                                                    </td>
+                                                </tr>
+
+                                             ) )
+                                             
+                                        ) : (
+
+                                            <tr>
+                                                <td colSpan={3}>No users</td>
+                                            </tr>
+                                        )
+
+                }
+
+   
+            </tbody>
+        </table>
+
+    );
+}
+ 
+export default UserTable;
